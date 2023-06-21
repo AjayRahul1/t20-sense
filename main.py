fb0c66f2b7fdd0597ad124e460a9beb0198ca82f
Ajay Rahul Prasad Punnamaraju <p.ajayrahul02@gmail.com>
2023-06-21 11:17:56 +0000

    requirements changed. Ground info added. Cloud Storage connected

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 from typing import List
 from fastapi.staticfiles import StaticFiles
-from ipl_func import get_particular_match_whole_score, get_series_from_year, get_match_ids_from_series_fast, get_match_info
+from ipl_func import get_particular_match_whole_score, get_series_from_year, get_match_ids_from_series_fast, get_match_info, get_all_csv_files_from_cloud, get_team_name_score_ground
 import pandas as pd
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+all_ipl_series_info = get_all_csv_files_from_cloud()
 matches_dict = {}
 matches_ids_list = []
 
@@ -42,15 +43,22 @@ async def process(
     print("Series ID: ", series_id)
     match_id = int(matches_dict[match_id])
 
+    toss_row_df = get_team_name_score_ground(series_id=series_id, match_id=match_id)
+    toss_row_ground = toss_row_df['ground'][0]
+
+
     batting1, bowling1, batting2, bowling2 = get_particular_match_whole_score(series_id, match_id)
     batting1 = batting1.to_dict(orient='records')
     bowling1 = bowling1.to_dict(orient='records')
     batting2 = batting2.to_dict(orient='records')
     bowling2 = bowling2.to_dict(orient='records')
 
+
+
     team1_name, team2_name, match_date, result, match_title = get_match_info(series_id, match_id)
     return templates.TemplateResponse("index.html", {   "request": request, "years" : years, "match_ids" : match_ids,
                                                         "batting1": batting1, "bowling1": bowling1,
                                                         "batting2": batting2, "bowling2": bowling2,
                                                         "team1_name": team1_name, "team2_name": team2_name,
-                                                        "match_date": match_date, "result": result, "match_title":match_title })
+                                                        "match_date": match_date, "result": result, "match_title":match_title,
+                                                        "ground_info": toss_row_ground })

5e4059443841773388435e4ede9a054dd5b45861
Ajay Rahul Prasad Punnamaraju <p.ajayrahul02@gmail.com>
2023-07-13 14:46:18 +0000

    1. JS code replaced with HTMX. 2. Tab Spaces changed from 4 to 2 for better spacing. 3. Column Names in Tables shortened. 4. Redirection to Error Pages working.

--- main.py
+++ main.py
@@ -12,36 +12,45 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 all_ipl_series_info = get_all_csv_files_from_cloud()
 matches_dict = {}
-matches_ids_list = []
+matches_names_list = []
 
 years = list(range(2008, 2024))  # Generate a list of years from 2008 to 2023
 match_ids = ["Select the Match"]  # Example match IDs
 
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
-    # years = [313494, 374163, 418064, 466304, 520932, 586733, 695871, 791129, 968923, 1078425, 1131611, 1165643, 1210595, 1249214, 1298423, 1345038]
-    finals_and_champs_df = pd.read_csv('Finals.csv')
-    finals_and_champs_df = finals_and_champs_df.to_dict(orient='records')
-    return templates.TemplateResponse("index.html", {"request": request, "years": years, "match_ids": match_ids, "finals_and_champs_df":finals_and_champs_df})
+  # years = [313494, 374163, 418064, 466304, 520932, 586733, 695871, 791129, 968923, 1078425, 1131611, 1165643, 1210595, 1249214, 1298423, 1345038]
+  finals_and_champs_df = pd.read_csv('Finals.csv')
+  finals_and_champs_df = finals_and_champs_df.to_dict(orient='records')
+  return templates.TemplateResponse("index.html", {"request": request, "years": years, "match_ids": match_ids, "finals_and_champs_df":finals_and_champs_df})
 
 @app.get("/get_match_ids")
 async def get_match_ids(year: int):
-    global matches_dict, match_ids_list
-    print(year)
-    if year == -2:
-        return JSONResponse(content=['Select a Match'])
-    series_id = get_series_from_year(year)
-    print(series_id)
-    matches_dict = get_match_ids_from_series_fast(series_id)  # Your function to retrieve match_ids based on series_id
-    match_ids_list = list(matches_dict.keys())
-    return JSONResponse(content=match_ids_list)
+  global matches_dict, match_names_list
+  print(year)
+  if year == -2:
+    return JSONResponse(content=['Select a Match'])
+  series_id = get_series_from_year(year)
+  print(series_id)
+  matches_dict = get_match_ids_from_series_fast(series_id)  # Your function to retrieve match_ids based on series_id
+  match_names_list = list(matches_dict.keys())
+  return JSONResponse(content=match_names_list)
 
-@app.get("/process", response_class=HTMLResponse)
+@app.get("/return_matches_names")
+async def ret_match_ids(year : int, request : Request):
+  global matches_dict, match_ids_list
+  series_id = get_series_from_year(year)
+  matches_dict = get_match_ids_from_series_fast(series_id)  # Your function to retrieve match_ids based on series_id
+  matches_names_list = list(matches_dict.keys())
+  return templates.TemplateResponse('content_loading_htmx/ipl_match_options.html', {"request" : request, "matches_names_list" : matches_names_list})
+      
+@app.get("/get_scorecard", response_class=HTMLResponse)
 async def process(
-        request: Request,
-        year: int,
-        match_name: str
-    ):
+    request: Request,
+    year: int,
+    match_name: str
+  ):
+  try:
     print("Year: ", year)
     print("Match Name: ", match_name)
     series_id = get_series_from_year(year)
@@ -69,13 +78,15 @@ async def process(
 
     team1_name, team2_name, match_date, result, match_title = get_match_info(series_id, match_id)
     return templates.TemplateResponse("index.html", {   "year": year, "match_id": match_id,
-                                                        "request": request, "years" : years, "match_ids" : match_ids,
-                                                        "batting1": batting1, "bowling1": bowling1,
-                                                        "batting2": batting2, "bowling2": bowling2,
-                                                        "team1_name": team1_name, "team2_name": team2_name,
-                                                        "match_date": match_date, "result": result, "match_title":match_title,
-                                                        "ground_info": ground_info, "toss_info": toss_info,
-                                                        "team1_name": team1_name, "team2_name":team2_name,
-                                                        "team1_score":team1_score, "team2_score":team2_score,
-                                                        "innings1_overs": innings1_overs, "innings2_overs": innings2_overs,
-                                                        "target" : team2_target })
+                              "request": request, "years" : years, "match_ids" : match_ids,
+                              "batting1": batting1, "bowling1": bowling1,
+                              "batting2": batting2, "bowling2": bowling2,
+                              "team1_name": team1_name, "team2_name": team2_name,
+                              "match_date": match_date, "result": result, "match_title":match_title,
+                              "ground_info": ground_info, "toss_info": toss_info,
+                              "team1_name": team1_name, "team2_name":team2_name,
+                              "team1_score":team1_score, "team2_score":team2_score,
+                              "innings1_overs": innings1_overs, "innings2_overs": innings2_overs,
+                              "target" : team2_target })
+  except:
+    return templates.TemplateResponse('error_pages/no_scorecard.html', {"request": request})
